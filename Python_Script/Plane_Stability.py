5f499a796743f9e21b8a9da7d575d4a5663c2af5
zeeshansalim1234 <59512700+zeeshansalim1234@users.noreply.github.com>
2020-11-09 01:44:27 -0700

    Update Plane_Stability.py

--- Python_Script/Plane_Stability.py
+++ Python_Script/Plane_Stability.py
@@ -1,4 +1,4 @@
-import time,pyautogui as pg
+import time, pyautogui as pg
 
 """---------------------------------------THIS MODULE IS FOR STABILITY ANALYSIS--------------------------------------"""
 
@@ -16,257 +16,299 @@ for line in f:
     if (countline >= 236 and countline <= 250 and countline % 2 == 0):
         array_inputs.append(line.strip())
 
-    elif (countline==253 or countline==256 or countline==259):
+    elif (countline == 253 or countline == 256 or countline == 259):
         array_inputs.append(line.strip())
 
-    elif (countline>=261 and countline <=265 and countline %2 != 0):
+    elif (countline >= 261 and countline <= 265 and countline % 2 != 0):
         array_inputs.append(line.strip())
 
-    elif (countline==268 or countline==271):
+    elif (countline == 268 or countline == 271):
         array_inputs.append(line.strip())
 
-for i in range(8,16):
+    elif (countline >= 273 and countline <= 279 and countline % 2 != 0):
+        array_inputs.append(line.strip())
 
-    if(i!=11 and i!=12 and i!=13):
+for i in range(8, 16):
 
-        array_inputs[i]=array_inputs[i].split(' ')
+    if (i != 11 and i != 12 and i != 13):
+        array_inputs[i] = array_inputs[i].split(' ')
 
 """---------------------------------------------------X--------------------------------------------------------------"""
 
 print("INPUTS:")
 print(array_inputs)
+time.sleep(0.5)
+pg.getWindowsWithTitle("xflr5 v6.47")[0].restore()
+
 
 def stabilityanalysis():
+    time.sleep(1)  # Analysis menu bar
+    pg.hotkey('Shift', 'F6')
 
-    time.sleep(5)                   # Analysis menu bar
-    pg.click(335,44)
-
-    time.sleep(0.5)                 # Stabilty analysis
-    pg.click(464,145)
-
-    fw=pg.getWindowsWithTitle('Stability Polar Definition')
-    fw[0].size=(775,540)
-    fw[0].topleft=(564,245)
+    time.sleep(1)
+    fw = pg.getWindowsWithTitle('Stability Polar Definition')
+    fw[0].size = (775, 540)
+    fw[0].topleft = (564, 245)
 
     """-------------------------------------------------ANALYSIS-----------------------------------------------------"""
 
-    time.sleep(0.5)                  # Click left arrow twice
-    pg.click(1275,366,clicks=2)
-
-    time.sleep(0.1)                  # Click Analysis menu bar
-    pg.click(634,368)
-
     time.sleep(0.5)
-    viscous_analysis_box=pg.locateOnScreen('viscousanalysis_checkbox.PNG')
+    viscous_analysis_box = pg.locateOnScreen('viscousanalysis_checkbox.PNG', region=(500, 240, 790, 550))
+
+    for i in range(0, 3):
+        pg.hotkey('shift', 'tab')
 
     if (array_inputs[0] == "y" or array_inputs[0] == "Y"):
 
-        if (viscous_analysis_box is None):                            # Check the box
+        if (viscous_analysis_box is None):  # Check the box
 
             time.sleep(0.1)
-            pg.click(613, 517)
+            pg.typewrite(['space'])
 
     elif (array_inputs[0] == "n" or array_inputs[0] == "N"):
 
-        if (viscous_analysis_box is not None):                        # Uncheck the box
+        if (viscous_analysis_box is not None):  # Uncheck the box
 
             time.sleep(0.1)
-            pg.click(613, 517)
+            pg.typewrite(['space'])
 
-    time.sleep(0.1)
-    pg.click(748, 615, clicks=3)                                   # Enter Beta
-    pg.typewrite(array_inputs[1])
-    pg.typewrite(["enter"])
+    for j in range(1, 3):
+        time.sleep(0.1)
+        pg.press('tab')
+        pg.typewrite(array_inputs[j])
 
-    time.sleep(0.1)
-    pg.click(748, 646, clicks=3)                                   # Enter Phi
-    pg.typewrite(array_inputs[2])
-    pg.typewrite(["enter"])
+    pg.press('tab', presses=4)
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """----------------------------------------------REF. DIMENSIONS-------------------------------------------------"""
 
-    time.sleep(0.1)                             # Ref Dimensions menu
-    pg.click(754, 366)
+    time.sleep(0.1)  # Ref Dimensions menu
+    pg.press('right')
+    pg.press('tab', presses=2)
 
     if (array_inputs[3] == '1'):
 
         time.sleep(0.1)
-        pg.click(635, 451)                      # Wing platform
+        pg.press('space')
 
     elif (array_inputs[3] == '2'):
 
         time.sleep(0.1)
-        pg.click(635, 483)                      # Wing platform projected on XY plane
+        pg.press('down')
+        pg.press('space')
 
     elif (array_inputs[3] == '3'):
 
         time.sleep(0.1)
-        pg.click(635, 514)                     # Manual input
+        pg.press('down', presses=2)
+        pg.press('space')
 
-        time.sleep(0.1)
-        pg.click(1222, 549, clicks=3)          # Enter Ref area
-        pg.typewrite(array_inputs[4])
+        for j in range(4, 7):
 
-        time.sleep(0.1)
-        pg.click(1222, 583, clicks=3)          # Enter Ref span length
-        pg.typewrite(array_inputs[5])
+            pg.press('tab')
+            pg.typewrite(array_inputs[j])
 
-        time.sleep(0.1)
-        pg.click(1222, 618, clicks=3)          # Enter Ref chord length
-        pg.typewrite(array_inputs[6])
+    pg.press('tab', presses=4)
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """---------------------------------------------MASS AND INERTIA-------------------------------------------------"""
 
-    time.sleep(0.1)                         # Mass and Inertia menu
-    pg.click(920, 365)
+    time.sleep(0.1)  # Mass and Inertia menu
+    pg.press('right')
 
-    useplaneinertia_box= pg.locateOnScreen('useplaneinertia_checkbox.PNG')
+    time.sleep(0.5)
+    useplaneinertia_box = pg.locateOnScreen('useplaneinertia_checkbox.PNG', region=(500, 240, 790, 550))
 
     if (array_inputs[7] == "y" or array_inputs[7] == "Y"):
 
-        if (useplaneinertia_box is None):                            # Check the box
+        if (useplaneinertia_box is None):  # Check the box
 
             time.sleep(0.1)
-            pg.click(613, 517)
+            pg.press('tab', presses=3)
+            pg.typewrite(['enter'])
+
+            for j in range(0, 2):
+                pg.hotkey('shift', 'tab')
+
+            pg.press('space')
+            pg.press('tab', presses=4)
+
 
     elif (array_inputs[7] == "n" or array_inputs[7] == "N"):
 
-        if (useplaneinertia_box is not None):                        # Uncheck the box
+        if (useplaneinertia_box is not None):  # Uncheck the box
 
             time.sleep(0.1)
-            pg.click(613, 517)
+            pg.press('tab', presses=2)
+            pg.press('space')
+            pg.press('tab', presses=7)
 
-    for i in range(0,14):
-
-
-        if(i<4):
-
-            x = 920
-            y = 480
+        else:
 
             time.sleep(0.1)
-            pg.click(x, y + (45 * (i)), clicks=2)
-            pg.typewrite(array_inputs[8][i])
-            pg.typewrite(["enter"])
+            pg.press('tab', presses=3)
 
-        elif(i<7):
+        for j in range(0, 7):
 
-            pg.typewrite(["down"])
-            pg.typewrite(array_inputs[i])
-            pg.typewrite(["enter"])
-
-        elif(i<14):
-
-            if (i == 7):
-
-                pg.typewrite(["right"])
-                pg.typewrite(array_inputs[9][i - 7])
-                pg.typewrite(["enter"])
+            if (j == 0):
+                pg.typewrite(array_inputs[8][0])
 
             else:
 
-                pg.typewrite(["up"])
-                pg.typewrite(array_inputs[9][i - 7])
-                pg.typewrite(["enter"])
+                pg.press('down')
+                pg.typewrite(array_inputs[8][j])
+
+        pg.press('tab')
+        pg.press('up', presses=6)
+
+        for j in range(0, 7):
+
+            if (j == 0):
+                pg.typewrite(array_inputs[9][0])
+
+            else:
+
+                pg.press('down')
+                pg.typewrite(array_inputs[9][j])
+
+        pg.press('enter',presses=2)
+        pg.press('tab',presses=2)
+
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """--------------------------------------------CONTROL PARAMETERS------------------------------------------------"""
 
     time.sleep(0.1)
-    pg.click(1058,367)                  # Control parameters menu
+    pg.press('right')
 
-    time.sleep(0.1)
-    pg.click(1267,452, clicks=3)        # Wing tilt
+    pg.press('tab', presses=3)
     pg.typewrite(array_inputs[10][0])
-    pg.typewrite(["enter"])
 
-    time.sleep(0.1)
-    pg.click(1267, 500, clicks=3)       #Elevator tilt
+    time.sleep(0.2)
+    pg.press('down')
     pg.typewrite(array_inputs[10][1])
-    pg.typewrite(["enter"])
+
+    time.sleep(0.2)
+    pg.press('enter', presses=2)
+    pg.press('tab', presses=2)
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """-----------------------------------------------AERO DATA------------------------------------------------------"""
 
     time.sleep(0.1)
-    pg.click(1211, 378)                 # Aero data menu
+    pg.press('right')
+    pg.press('tab', presses=2)
 
-    if(array_inputs[11]=="int"):
+    if (array_inputs[11] == "int"):
 
-        time.sleep(0.1)                 # International radio button
-        pg.click(669,439)
+        time.sleep(0.1)  # International radio button
+        pg.press('left')
 
-    elif(array_inputs[11]=="imp"):
+    elif (array_inputs[11] == "imp"):
 
-        time.sleep(0.1)                 # Imperial radio button
-        pg.click(799, 440)
+        time.sleep(0.1)  # Imperial radio button
+        pg.press('right')
 
-    time.sleep(0.1)
-    pg.click(964, 475, clicks=3)       # Enter Rho
-    pg.typewrite(array_inputs[12])
+    for j in range(12, 14):
+        pg.press('tab')
+        pg.typewrite(array_inputs[j])
 
-    time.sleep(0.1)
-    pg.click(957, 505, clicks=3)       # Enter Mu
-    pg.typewrite(array_inputs[13])
+    pg.press('tab', presses=5)
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """-----------------------------------------------EXTRA DRAG-----------------------------------------------------"""
 
-    time.sleep(0.1)                   # Click right arrow
-    pg.click(1296,368)
+    time.sleep(0.1)  # Click right arrow
+    pg.press('right')
 
-    time.sleep(0.1)                   # Extra drag menu
-    pg.click(1211,362)
+    pg.press('tab', presses=3)
 
-    for i in range(0,8):
+    for k in range(14, 16):
 
-         y=475
+        for j in range(0, 4):
 
-         if(i<4):                      # To Fill extra area
+            if (j == 0):
 
-             x=1045
+                pg.typewrite(array_inputs[k][j])
 
-             time.sleep(0.1)
-             pg.click(x,y+(45*(i)), clicks=3)
-             pg.typewrite(array_inputs[14][i])
-             pg.typewrite(["enter"])
+            else:
 
-         else:                          # To Fill extra drag coeff.
+                pg.press('down')
+                pg.typewrite(array_inputs[k][j])
 
-             x=1250
+        pg.press('tab')
+        pg.press('up', presses=3)
 
-             time.sleep(0.1)
-             pg.click(x, y+(45*(i-4)), clicks=3)
-             pg.typewrite(array_inputs[14][i-4])
-             pg.typewrite(["enter"])
+    pg.press('enter', presses=3)
 
-    for i in range(0, 2):
-        pg.typewrite(["enter"])
 
-    time.sleep(0.5)                         # Save
-    pg.click(1137, 742)
 
     """---------------------------------------------------X----------------------------------------------------------"""
 
     """-------------------------------------------PlANE ANALYSIS MENU------------------------------------------------"""
 
+    time.sleep(1)
+    fw1 = pg.getWindowsWithTitle("Plane analysis")
+
+    fw1[0].size = (309, 871)  # Assures window is at the same place for all users
+    fw1[0].topleft = (1615, 114)
+
     time.sleep(0.5)
-    sequence_locator = pg.locateOnScreen('sequence_checkbox.PNG')
+    sequence_on = pg.locateOnScreen('sequence_on.PNG', region=(1400, 0, 400, 400))
+    sequence_off = pg.locateOnScreen('sequence_off.PNG', region=(1400, 0, 400, 400))
+    pg.press('enter')
 
-    if(sequence_locator is not None):
+    if (sequence_on is not None):
 
-        time.sleep(0.5)
-        pg.click(1661, 217)
+        for j in range(0,5):
 
-    time.sleep(0.5)                  # Analyze
-    pg.click(1767, 415)
+            pg.hotkey('shift','tab')
+
+        if (array_inputs[16] == 'y' or array_inputs[16] == 'Y'):
+
+            for j in range(17,20):
+
+                pg.press('tab')
+                pg.typewrite(array_inputs[j])
+
+            pg.press('tab',presses=2)
+            pg.typewrite(['space'])
+
+        if (array_inputs[16] == 'n' or array_inputs[16] == 'N'):
+
+            pg.typewrite(['space'])
+            pg.press('tab',presses=3)
+            pg.typewrite(['space'])
+
+
+    elif (sequence_off is not None):
+
+        for j in range(0,3):
+
+            pg.hotkey('shift','tab')
+
+        if (array_inputs[16] == 'y' or array_inputs[16] == 'Y'):
+
+            pg.typewrite(['space'])
+
+            for j in range(17,20):
+
+                pg.press('tab')
+                pg.typewrite(array_inputs[j])
+
+            pg.press('tab',presses=2)
+            pg.typewrite(['space'])
+
+
+        if (array_inputs[16] == 'n' or array_inputs[16] == 'N'):
+
+            pg.press('tab',presses=3)
+            pg.typewrite(['space'])
 
 
 """----------------------------------------------END OF FUNCTION-----------------------------------------------------"""
