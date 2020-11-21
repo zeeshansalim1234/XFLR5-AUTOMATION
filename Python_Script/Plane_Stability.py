49f89f261dbede401e3c9c2561cf752e589cf7ff
zeeshansalim1234 <59512700+zeeshansalim1234@users.noreply.github.com>
2020-11-21 10:16:27 -0700

    Added GUI
    GUI added for user inputs

--- Python_Script/Plane_Stability.py
+++ Python_Script/Plane_Stability.py
@@ -1,6 +1,245 @@
 import time, pyautogui as pg
+from tkinter import *
+from PIL import ImageTk,Image
 
 """---------------------------------------THIS MODULE IS FOR STABILITY ANALYSIS--------------------------------------"""
+
+"""----------------------------------------------------GUI-----------------------------------------------------------"""
+
+arr_gui=[]
+
+def save_info():
+
+    arr_gui.append(firstentry.get())
+    arr_gui.append(secondentry.get())
+    arr_gui.append(thirdentry.get())
+    arr_gui.append(fourthentry.get())
+    arr_gui.append(fifthentry.get())
+    arr_gui.append(sixthentry.get())
+    arr_gui.append(seventhentry.get())
+    arr_gui.append(eighthentry.get())
+    arr_gui.append(ninthentry.get())
+    arr_gui.append(tenthentry.get())
+    arr_gui.append(eleventhentry.get())
+    arr_gui.append(twelvethentry.get())
+    arr_gui.append(thirteenthentry.get())
+    arr_gui.append(fourteenthentry.get())
+    arr_gui.append(fifteenthentry.get())
+    arr_gui.append(sixteenthentry.get())
+    arr_gui.append(seventeenthentry.get())
+    arr_gui.append(eighteenthentry.get())
+    arr_gui.append(nineteenthentry.get())
+    arr_gui.append(twenteethentry.get())
+
+
+
+
+    print(arr_gui)
+
+    f = open("XFLR5inputs.txt", "r")
+    lines = f.readlines()
+
+    x=0
+
+    for k in range(235,250,2):
+
+        if (arr_gui[x] != ''):
+            lines[k] =str(arr_gui[x])+'\n'
+
+        x+=1
+
+    for k in range(252,259,3):
+
+        if (arr_gui[x] != ''):
+            lines[k] =str(arr_gui[x])+'\n'
+
+        x+=1
+
+    for k in range(260,265,2):
+
+        if (arr_gui[x] != ''):
+            lines[k] =str(arr_gui[x])+'\n'
+
+        x+=1
+
+    for k in range(267,271,3):
+
+        if (arr_gui[x] != ''):
+            lines[k] =str(arr_gui[x])+'\n'
+
+        x+=1
+
+    for k in range(272,279,2):
+
+        if (arr_gui[x] != ''):
+            lines[k] =str(arr_gui[x])+'\n'
+
+        x+=1
+
+    f.close()
+
+    lines = ''.join([str(elem) for elem in lines])
+
+
+    f = open("XFLR5inputs.txt", "w")
+
+    f.writelines(lines)
+    f.close()
+
+
+
+app = Tk()
+
+app.geometry("1900x1500")
+
+app.title("XFLR5 AUTOMATION")
+
+app.configure(background="#F7F7F7")
+
+heading = Label(text="STABILITY ANALYSIS",fg="white",bg="black",font="Helvetica 13 bold italic",width="500",height="3")
+
+heading.pack()
+
+
+img1 = ImageTk.PhotoImage(Image.open('university_logo.png'))
+label=Label(image=img1,bg="#F7F7F7")
+label.pack()
+label.place(x=1700,y=820)
+
+img2 = ImageTk.PhotoImage(Image.open('SAED_logo.jpg'))
+label=Label(image=img2,bg="#F7F7F7")
+label.pack()
+label.place(x=1350,y=870)
+
+
+firstentry_text = Label(text="Viscous Anlaysis check box ON in analysis menu(y/n)?")
+secondentry_text = Label(text="Enter Beta value for flight altitude:")
+thirdentry_text = Label(text="Enter Phi value for flight altitude:")
+fourthentry_text =Label(text="Select Wing Platform (1)/Wing Platform projected on xy plane/Manual input")
+fifthentry_text =Label(text="Enter Ref. area (Only if you chose Manual input):")
+sixthentry_text = Label(text="Enter Ref. span length (Only if you chose Manual input):")
+seventhentry_text = Label(text="Enter Ref. cord length (Only if you chose Manual input):")
+eighthentry_text = Label(text="Use plane inertia check box ON in Mass and inertia menu(y/n)?")
+ninthentry_text = Label(text="Enter mean value of inertia parameters CoG_x CoG_y Ixx Iyy Izz Ixz")
+tenthentry_text = Label(text="Enter Gain(unit/ctrl)of parameters Mass CoG_x CoG_y Ixx Iyy Izz Ixz")
+eleventhentry_text = Label(text="Enter Gain(Â°/ctrl) for control parameters Wing_Tilt Elevator_Tilt")
+twelvethentry_text = Label(text="Choose international or imperial for Air Data menu (enter int or imp):")
+thirteenthentry_text = Label(text="Enter the value of Rho:")
+fourteenthentry_text = Label(text="Enter the value of Mu:")
+fifteenthentry_text= Label(text="Enter Extra Area(m^2) for Extra drag table")
+sixteenthentry_text= Label(text="Enter Extra drag coeff. for EXtra drag table")
+seventeenthentry_text= Label(text="Sequence ON or OFF(y/n)?")
+eighteenthentry_text= Label(text="Enter Start value:")
+nineteenthentry_text= Label(text="Enter End value:")
+twenteethentry_text= Label(text="Enter increment value:")
+
+
+firstentry_text.place(x=15,y=110)
+secondentry_text.place(x=15,y=160)
+thirdentry_text.place(x=15,y=210)
+fourthentry_text.place(x=15,y=260)
+fifthentry_text.place(x=15,y=310)
+sixthentry_text.place(x=15,y=360)
+seventhentry_text.place(x=15,y=410)
+eighthentry_text.place(x=15,y=460)
+ninthentry_text.place(x=15,y=510)
+tenthentry_text.place(x=15,y=560)
+eleventhentry_text.place(x=15,y=610)
+twelvethentry_text.place(x=15,y=660)
+thirteenthentry_text.place(x=15,y=710)
+fourteenthentry_text.place(x=15,y=760)
+fifteenthentry_text.place(x=560,y=110)
+sixteenthentry_text.place(x=560,y=160)
+seventeenthentry_text.place(x=560,y=310)
+eighteenthentry_text.place(x=560,y=360)
+nineteenthentry_text.place(x=560,y=410)
+twenteethentry_text.place(x=560,y=460)
+
+
+
+firstentry = StringVar()
+secondentry = StringVar()
+thirdentry = StringVar()
+fourthentry= StringVar()
+fifthentry= StringVar()
+sixthentry= StringVar()
+seventhentry=StringVar()
+eighthentry=StringVar()
+ninthentry=StringVar()
+tenthentry=StringVar()
+eleventhentry=StringVar()
+twelvethentry=StringVar()
+thirteenthentry=StringVar()
+fourteenthentry=StringVar()
+fifteenthentry=StringVar()
+sixteenthentry=StringVar()
+seventeenthentry=StringVar()
+eighteenthentry=StringVar()
+nineteenthentry=StringVar()
+twenteethentry=StringVar()
+
+
+
+firstentry_enter = Entry(textvariable=firstentry,width="30")
+secondentry_enter = Entry(textvariable=secondentry,width="30")
+thirdentry_enter = Entry(textvariable=thirdentry,width="30")
+fourthentry_enter = Entry(textvariable=fourthentry,width="30")
+fifthentry_enter = Entry(textvariable=fifthentry,width="30")
+sixthentry_enter = Entry(textvariable=sixthentry,width="30")
+seventhentry_enter= Entry(textvariable=seventhentry,width="30")
+eighthentry_enter= Entry(textvariable=eighthentry,width="30")
+ninthentry_enter= Entry(textvariable=ninthentry,width="30")
+tenthentry_enter= Entry(textvariable=tenthentry,width="30")
+eleventhentry_enter= Entry(textvariable=eleventhentry,width="30")
+twelvethentry_enter= Entry(textvariable=twelvethentry,width="30")
+thirteenthentry_enter= Entry(textvariable=thirteenthentry,width="30")
+fourteenthentry_enter= Entry(textvariable=fourteenthentry,width="30")
+fifteenthentry_enter=Entry(textvariable=fifteenthentry,width="30")
+sixteenthentry_enter= Entry(textvariable=sixteenthentry,width="30")
+seventeenthentry_enter= Entry(textvariable=seventeenthentry,width="30")
+eighteenthentry_enter= Entry(textvariable=eighteenthentry,width="30")
+nineteenthentry_enter= Entry(textvariable=nineteenthentry,width="30")
+twenteethentry_enter= Entry(textvariable=twenteethentry,width="30")
+
+
+
+firstentry_enter.place(x=15,y=140)
+secondentry_enter.place(x=15,y=190)
+thirdentry_enter.place(x=15,y=240)
+fourthentry_enter.place(x=15,y=290)
+fifthentry_enter.place(x=15,y=340)
+sixthentry_enter.place(x=15,y=390)
+seventhentry_enter.place(x=15,y=440)
+eighthentry_enter.place(x=15,y=490)
+ninthentry_enter.place(x=15,y=540)
+tenthentry_enter.place(x=15,y=590)
+eleventhentry_enter.place(x=15,y=640)
+twelvethentry_enter.place(x=15,y=690)
+thirteenthentry_enter.place(x=15,y=740)
+fourteenthentry_enter.place(x=15,y=790)
+fifteenthentry_enter.place(x=560,y=140)
+sixteenthentry_enter.place(x=560,y=190)
+seventeenthentry_enter.place(x=560,y=340)
+eighteenthentry_enter.place(x=560,y=390)
+nineteenthentry_enter.place(x=560,y=440)
+twenteethentry_enter.place(x=560,y=490)
+
+
+
+button1 = Button(app,text="Save",command=save_info,width="30",height="2",bg="orange")
+button1.place(x=15,y=900)
+
+
+
+
+
+mainloop()
+
+
+
+
+"""------------------------------------------------------X-----------------------------------------------------------"""
+
 
 """--------------------------------------------READING FROM TEXT FILE------------------------------------------------"""
 
